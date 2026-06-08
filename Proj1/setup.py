a734e099225f148eb4380d721b988e2eadc27ddb
Saurav <sauravnigam9752@gmail.com>
2026-06-08 16:26:50 +0530

    Convert Vehicle_Insurance from submodule to regular folder

---
+++ Proj1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saurav Nigam",
+    author_email="sauravnigam9752@gmail.com",
+    packages=find_packages()
+)
